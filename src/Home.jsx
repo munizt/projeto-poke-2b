5c26470a93b0b8d0d695db4b12bfb0415f7e25e0
munizt <gabrielmuniz.6419@gmail.com>
2025-05-05 12:29:25 -0300

    img.pke

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import poke from "./assets/pokeapi_256.png"
+
+const Home = ()=> {
+
+    return (
+        <div className="pokeflex">
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            <img src={poke} />
+            
+        
+        </div>
+    )
+}
+
+export default Home 
